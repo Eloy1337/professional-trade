541f225b10e707a94113a5afcb39437eaa802560
Aslan <tuesday@yandex-team.ru>
2021-11-17 21:00:58 +0300

    Добавил новые статьи и немного правил код

--- src/components/Paper/index.js
+++ src/components/Paper/index.js
@@ -1,23 +1,35 @@
-import styles from "./Paper.module.scss";
+import styles from './Paper.module.scss';
 
 function Paper({ name, img, text, time, author, onClickPaper }) {
-	return (
-		<article onClick={onClickPaper} className={styles.article}>
-			<section className={styles.body}>
-				<section className={styles.bodyLeft}>
-					<section className={styles.title}>
-						<h3>{name}</h3>
-					</section>
-					<img src={img} alt="articleimg" />
-				</section>
-				<section className={styles.bodyRight}>
-					<span className={styles.author} style={{ backgroundImage: "url('icons/user.svg')" }}>{author}</span>
-					<span className={styles.time} style={{ backgroundImage: "url('icons/timer-dark.svg')" }}>{time}</span>
-					<p><pre>{text}</pre></p>
-				</section>
-			</section>
-		</article>
-	)
+  return (
+    <article onClick={onClickPaper} className={styles.article}>
+      <section className={styles.body}>
+        <section className={styles.bodyLeft}>
+          <section className={styles.title}>
+            <h3>{name}</h3>
+          </section>
+          <img src={img} alt='articleimg' />
+        </section>
+        <section className={styles.bodyRight}>
+          <span
+            className={styles.author}
+            style={{ backgroundImage: "url('icons/user.svg')" }}
+          >
+            {author}
+          </span>
+          <span
+            className={styles.time}
+            style={{ backgroundImage: "url('icons/timer-dark.svg')" }}
+          >
+            {time}
+          </span>
+          <p>
+            <pre>{text}</pre>
+          </p>
+        </section>
+      </section>
+    </article>
+  );
 }
 
 export default Paper;
--- src/components/PaperPopup.js
+++ src/components/PaperPopup.js
@@ -1,27 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-function PaperPopup({ name, text, onLoad, onClose }) {
-	useEffect(() => {
-		onLoad("" + document.querySelector(".paperPopup")?.clientHeight + "px");
-		document.querySelector(".wrapper").scrollIntoView({ behavior: "smooth" });
-	});
+function PaperPopup({ name, text, img, onLoad, onClose }) {
+  useEffect(() => {
+    onLoad('' + document.querySelector('.paperPopup')?.clientHeight + 'px');
+    document.querySelector('.wrapper').scrollIntoView({ behavior: 'smooth' });
+  });
 
-	return (
-		<section className="paperPopup">
-			<div className="container">
-				<section className="title">
-					<section className="info">
-						<h2>{name}</h2>
-						{/* <span>author</span> */}
-					</section>
-					<button onClick={onClose}>✕</button>
-				</section>
-				<section className="text">
-					<pre>{text}</pre>
-				</section>
-			</div>
-		</section>
-	);
+  return (
+    <section className='paperPopup'>
+      <div className='container'>
+        <section className='title'>
+          <section className='info'>
+            <h2>{name}</h2>
+          </section>
+          <button onClick={onClose}>✕</button>
+        </section>
+        <section className='mainImg'>
+          <img src={img[0]} alt='img' />
+        </section>
+        <section className='text'>
+          <pre>{text}</pre>
+        </section>
+        {img[1] && (
+          <section className='images'>
+            {img.map((item) => {
+              if (item === img[0]) {
+                return false;
+              }
+              return <img src={item} alt='img' />;
+            })}
+          </section>
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default PaperPopup;
